ede20b90a1f309ab8d453b0f8ad1e8d6b33ea91b
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-04-07 09:35:25 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     rwMode = 1 - rwMode
-    i2ctester()
+    lowAddress = 0
+    basic.clearScreen()
 })
 function startCondition () {
     pins.digitalWritePin(sclPin, 1)
@@ -22,11 +23,14 @@ function daraIn () {
     return inData
 }
 input.onButtonPressed(Button.A, function () {
-    highAddress = (highAddress - 1) % 8
-    i2ctester()
+    highAddress += -1
+    if (highAddress < 0) {
+        highAddress = 7
+    }
+    lowAddress = 0
+    basic.clearScreen()
 })
 function dispAddress (address: number) {
-    basic.clearScreen()
     for (let bitPosition = 0; bitPosition <= 2; bitPosition++) {
         if ((address >> bitPosition) & 0x01) {
             led.plot(4 - bitPosition, 0)
@@ -46,8 +50,12 @@ function i2cTest (rwMode: number, address: number) {
     return inData
 }
 input.onButtonPressed(Button.B, function () {
-    highAddress = (highAddress + 1) % 8
-    i2ctester()
+    highAddress += 1
+    if (highAddress > 7) {
+        highAddress = 0
+    }
+    lowAddress = 0
+    basic.clearScreen()
 })
 function dataOut (data: number) {
     pins.digitalWritePin(sdaPin, data)
@@ -57,19 +65,6 @@ function dataOut (data: number) {
     control.waitMicros(5)
     pins.digitalWritePin(sclPin, 0)
     control.waitMicros(5)
-}
-function i2ctester () {
-    dispAddress(highAddress)
-    for (let lowAddress = 0; lowAddress <= 15; lowAddress++) {
-        if (!(highAddress == 0 && lowAddress == 0 || highAddress == 7 && lowAddress == 15)) {
-            if (i2cTest(rwMode, highAddress * 16 + lowAddress)) {
-                led.plotBrightness(lowAddress % 4 + 1, lowAddress / 4 + 1, brightOff)
-            } else {
-                led.plotBrightness(lowAddress % 4 + 1, lowAddress / 4 + 1, brightOn)
-            }
-        }
-        basic.pause(20)
-    }
 }
 function stopCondition () {
     control.waitMicros(5)
@@ -94,21 +89,37 @@ function ackWait () {
     dataOut(nack)
     return inData
 }
+let i2cAddress = 0
 let inData = 0
+let lowAddress = 0
 let rwMode = 0
 let nack = 0
-let brightOff = 0
-let brightOn = 0
 let highAddress = 0
 let sdaPin = 0
 let sclPin = 0
+serial.redirectToUSB()
 let ack = 0
 sclPin = DigitalPin.P2
 sdaPin = DigitalPin.P16
 pins.setPull(sdaPin, PinPullMode.PullUp)
 pins.digitalWritePin(sclPin, 1)
 highAddress = 0
-brightOn = 255
-brightOff = 10
+let brightOn = 255
+let brightOff = 10
 nack = 1
-i2ctester()
+basic.forever(function () {
+    dispAddress(highAddress)
+    i2cAddress = highAddress * 16 + lowAddress
+    if (i2cAddress != 0 && i2cAddress != 127) {
+        serial.writeLine(convertToText(i2cAddress))
+        if (i2cTest(rwMode, i2cAddress)) {
+            led.plotBrightness(lowAddress % 4 + 1, lowAddress / 4 + 1, brightOff)
+        } else {
+            led.plotBrightness(lowAddress % 4 + 1, lowAddress / 4 + 1, brightOn)
+        }
+    } else {
+        led.unplot(lowAddress % 4 + 1, lowAddress / 4 + 1)
+    }
+    lowAddress = (lowAddress + 1) % 16
+    basic.pause(20)
+})
